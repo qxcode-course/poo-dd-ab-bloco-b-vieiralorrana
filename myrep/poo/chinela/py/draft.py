4be7ce8b291dac04de4d6137c9e147e7ded8acd5
Lorrana <lorranav905@gmail.com>
2025-10-06 17:19:38 -0300

    atividade da chinela com o loop funcionando, comecei em sala e trouxe a main pra fazer em casa

--- myrep/poo/chinela/py/draft.py
+++ myrep/poo/chinela/py/draft.py
@@ -6,10 +6,16 @@ class Chinela:
         return self.__tamanho
     
     def setTam(self, valor: int):
-        if valor < 20 or valor > 50 and valor % 2 != 0:
+        if valor < 20 or valor > 50 or valor % 2 != 0:
             print("fail: tamanho invalido")
         else:
             self.__tamanho = valor
+
+    def show(self) -> None:
+        print(self)
+    
+    def __str__(self): 
+        return (f"O tamanho da minha chinela é: {self.getTam()}")
 
 def main():
     chinela = Chinela()
@@ -17,11 +23,15 @@ def main():
     while True:
         line = input()
         print("$" + line)
-        args: list[str] = line
+        args = line.split(" ")
         
-        if args[0] == "init":
-            valor = args[1]
+        if args[0] == "end":
+            break
+        elif args[0] == "init":
+            valor = int(args[1])
             chinela.setTam(valor)
+        elif args[0] == "show":
+            print(chinela)
 
 main()
             
